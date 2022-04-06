9d4c8055d7c4c691da5a2456872f652366ffe0bf
Plehov Evgeny <plehov1992@yandex.ru>
2022-04-06 19:55:50 +0500

    Fix compile error

--- src/main/java/com/plehov/Main.java
+++ src/main/java/com/plehov/Main.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Ура мы подключили");
-        dsfvgsdv2 := 1;
     }
 }
